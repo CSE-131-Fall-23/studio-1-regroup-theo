eccf8d59f350fa5e360e6954ddd97f2807e9ab16
telfe <telfe@LAPTOP-2QP0PD20.dhcp.wustl.edu>
2023-09-06 16:50:51 -0500

    This was fun

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -17,6 +17,8 @@ public class HiFour {
 		String s2 = in.nextLine();
 		System.out.print("Enter value for arg 3: ");
 		String s3 = in.nextLine();
+		
+		System.out.println("Hi, " + s0 + ", and " + s1 + ", and " + s2 + ", and " + s3 + ". How are you all?");
 		//
 		// Say hello to the names in s0 through s3.
 		//
